a00ed749bce8efdb5f67211cfcbff5d5a66704f6
rgiusti <rgiusti@alabamasolutions.com>
2022-07-17 17:12:15 -0300

    mock: efecto telón para inicio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromMain from './store'
 import * as fromPhotoActions from './modules/photo/store/actions/photo.actions'
 import * as fromInteriorActions from './modules/interior/store/actions/interior.actions'
 import { FireService } from './services/fire.service';
+import { of } from 'rxjs';
+import { take } from 'rxjs/operators';
+// import Scrollbar from 'smooth-scrollbar';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'desenia-app';
   constructor(private store: Store<fromMain.State>, private fire: FireService){
     this.store.dispatch(fromPhotoActions.loadPhotos());
     // this.store.dispatch(fromInteriorActions.loadInteriors());
-
-
+    document.addEventListener('scroll', this.splitScreen);
   }
+  splitScreen(event: Event): void {
+    let split = document.getElementsByClassName('main-content');    
+    (split[0] as HTMLElement).style.transform = `translateX(-${Math.ceil(window.scrollY)}px) translateY(${Math.ceil(window.scrollY)}px)`;
+    (split[1] as HTMLElement).style.transform = `translateX(${Math.ceil(window.scrollY)}px) translateY(${Math.ceil(window.scrollY)}px)`
+    
+  }
+
+  ngOnInit(): void {    
+  }
+
 }
