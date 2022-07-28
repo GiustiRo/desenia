a50013f620e9212f4758807f9b900737dcc52ed1
GiustiRo <bigbeeh0es3@gmail.com>
2022-07-28 00:44:10 -0300

    mock: scroll horizontal formula y animación svg.

--- src/app/modules/shared/components/desenia-frames/desenia-frames.component.ts
+++ src/app/modules/shared/components/desenia-frames/desenia-frames.component.ts
@@ -1,23 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-desenia-frames',
   templateUrl: './desenia-frames.component.html',
   styleUrls: ['./desenia-frames.component.scss']
 })
 export class DeseniaFramesComponent implements OnInit {
-  showContent:boolean = false;
-  stylesList: string[] = ['a','b','c'];
+  showContent: boolean = false;
+  stylesList: string[] = ['a', 'b', 'c'];
   pickStyle!: string
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit(): void {
-    let rand = Math.floor(Math.random() * 3);    
+    let rand = Math.floor(Math.random() * 3);
     this.pickStyle = this.stylesList[rand];
+
   }
 
-  toggleShowContent(): void{
+  toggleShowContent(): void {
+    console.warn('scroll!');
     this.showContent = true;
+    let sc = (document.getElementById('stick-content') as HTMLDivElement).getBoundingClientRect();
+    console.warn(sc);
+
+    function smoothScroll(target: any, duration: any) {
+      var target = document.querySelector(target);
+      var targetPosition = target.getBoundingClientRect().width + target.getBoundingClientRect().width;
+      var startPosition = target.scrollLeft;
+      var distance = targetPosition// - startPosition;
+      var startTime: any = null;
+
+      function animation(currentTime: any) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        target.scrollTo(run, 0);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+      }
+
+      // http://www.gizma.com/easing/
+      function ease(t: any, b: any, c: any, d: any) {
+        t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+
+      }
+      requestAnimationFrame(animation);
+    }
+    smoothScroll('#stick-content', 5000)
+    document.querySelector('#nav-to-left')?.classList.add('anim-cursiva');
+    document.querySelectorAll('.path-ashes').forEach(el => el.classList.add('anim-ashes'))
+
   }
 
+  getScroll(): void {
+    console.warn('scrolling??');
+
+  }
 }
