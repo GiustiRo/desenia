c9f3a885cdef597fa0a6b53a7c35e1aefeb1062d
rgiusti <rgiusti@alabamasolutions.com>
2022-07-31 16:52:20 -0300

    mock: index boxes. nuevo svg.

--- src/app/modules/shared/components/desenia-frames/desenia-frames.component.ts
+++ src/app/modules/shared/components/desenia-frames/desenia-frames.component.ts
@@ -12,22 +12,47 @@ export class DeseniaFramesComponent implements OnInit {
   stylesList: string[] = ['a', 'b', 'c'];
   pickStyle!: string;
 
+  indexBoxes: any[] = [];
+
   constructor(
     private scrollSv: ScrollService
   ) { }
 
   ngOnInit(): void {
-    let rand = Math.floor(Math.random() * 3);
+    let rand = 2;//Math.floor(Math.random() * 3);
     this.pickStyle = this.stylesList[rand];
 
     window.addEventListener('scroll', this.hzScroll);
+
+    this.indexBoxes = [
+      {
+        title: 'Le Petit Brisé',
+        shortDesc: 'Cabaña en el cerror de materiales orgánicos.',
+        materialUrl: 'https://static.vecteezy.com/system/resources/thumbnails/003/304/627/small/wood-texture-background-free-vector.jpg'
+      },
+      {
+        title: 'Cocina "Susanne"',
+        shortDesc: 'Lorem ipsum via aqua most petiern amen.',
+        materialUrl: 'https://i.pinimg.com/236x/11/e0/4e/11e04efd71926730d3e2c730f407ce68--material.jpg'
+      },
+      {
+        title: 'Room & Shower',
+        shortDesc: 'Lorem ipsum via aqua most petiern amen.',
+        materialUrl: 'https://static.vecteezy.com/system/resources/thumbnails/005/461/026/small/seamless-floral-pattern-with-leaves-on-a-green-background-natural-texture-for-eco-design-of-textiles-wallpaper-printing-on-paper-fabric-scrapbooking-vector.jpg'
+      },
+      {
+        title: 'Cielo Violeta',
+        shortDesc: 'Lorem ipsum via aqua most petiern amen.',
+        materialUrl: 'https://designscad.com/wp-content/uploads/2017/01/brick_wall_texture_2d_bmp_graphics_graphics_81003.jpg'
+      }
+    ]
   }
 
   async hzScroll(event: Event): Promise<void | boolean> {
     let thresholdValue = 1400;
 
     let calcStrokeMove = (reset?: boolean) => {
-      let multipy = 6;
+      let multipy = 8;
       let calcAshes = window.scrollY - thresholdValue;
       // console.warn('ashes::::', calcAshes);
       document.querySelectorAll('.path-ashes').forEach((el) => {
@@ -38,7 +63,7 @@ export class DeseniaFramesComponent implements OnInit {
         (el as SVGPathElement).style.strokeDashoffset = `${calcAshes * multipy}px`;
         (el as SVGPathElement).style.strokeDasharray = `${calcAshes * multipy < 2800 ? 2800 : calcAshes * multipy}px`;
       });
-      (document.querySelector('#draw-path') as SVGPathElement).style.strokeDashoffset = `${(reset ? 0 : calcAshes * 1.3) + 5000}`;
+      (document.querySelector('#draw-path') as SVGPathElement).style.strokeDashoffset = `${-(reset ? 0 : calcAshes * 1.3) + 15500}`;
     }
 
     // TRIGGER SCROLL
